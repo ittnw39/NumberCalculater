75ac3874478bfe8b210a4b46b8a514e011ad27f2
ittnw39 <ittnw39@gmail.com>
2025-08-18 00:44:37 +0900

    250817: 버전 명시

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     id("java")
     id("application")
     id("org.openjfx.javafxplugin") version "0.1.0"
+    id("org.beryx.jlink") version "2.26.0"
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "25.08.17"
 
 repositories {
     mavenCentral()
@@ -27,5 +28,27 @@ javafx {
 
 application {
     mainClass.set("org.example.NumberCalculatorApp")
-    applicationName = "숫자 분할 계산기"
+    applicationName = "길이 분할 계산기"
+}
+
+// jlink 설정
+jlink {
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+    launcher {
+        name = "길이 분할 계산기"
+        mainClass.set("org.example.NumberCalculatorApp")
+    }
+    jpackage {
+        installerType = "exe"
+        installerOptions = listOf(
+            "--win-per-user-install",
+            "--win-dir-chooser",
+            "--win-menu",
+            "--win-shortcut"
+        )
+        appVersion = project.version.toString()
+        appName = "길이 분할 계산기"
+        vendor = "Jinwoo"
+        icon = "src/main/resources/icon.ico"
+    }
 }
